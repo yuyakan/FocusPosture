6a44fe179c6fd4900c4eeb510a2667fef15901b9
yuyakan <kanbe1365@icloud.com>
2025-08-30 14:49:26 +0900

    とりあえず動く状態

---
+++ Balance/BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceApp.swift
+//  Balance
+//
+//  Created by 上別縄祐也 on 2025/08/30.
+//
+
+import SwiftUI
+
+@main
+struct BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeasurementView()
+        }
+    }
+}
---
+++ BalanceTests/BalanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceTests.swift
+//  BalanceTests
+//
+//  Created by 上別縄祐也 on 2025/08/30.
+//
+
+import Testing
+@testable import Balance
+
+struct BalanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
